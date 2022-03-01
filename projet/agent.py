246fef1e142ee870d50ae7837dd2a302ed3803c4
Thibault Pech <thibaultpech@outlook.com>
2022-03-01 10:42:22 +0100

    bf socket

--- projet/agent.py
+++ projet/agent.py
@@ -10,21 +10,33 @@ import mysql.connector
 import psutil
 import time
 import shutil
+import os
+import csv
 
 
 def seconds_elapsed():
     return time.time() - psutil.boot_time()
 
+total, used, free = shutil.disk_usage("/")
+
+with open('services.csv', 'r') as fd:
+    reader = csv.reader(fd)
+    for row in reader:
+        # do something
+        print("oui")
+
+
 mydb = mysql.connector.connect(
-    host="192.168.43.197",
+    host="192.168.243.23",
     user="caiomi",
     database="supervisionpython"
 )
 
 mycursor = mydb.cursor()
 
-sql = "INSERT INTO info_pc (hostname, OS_NAME, uptime, kernel, CPUname, CPUfrequency) VALUES (%s, %s, %s, %s, %s, %s);"
-val = (str(socket.gethostname()), str(platform.system()), float(seconds_elapsed()), str(platform.release()), str(cpuinfo.get_cpu_info()["brand_raw"]), float(str(cpuinfo.get_cpu_info()["hz_actual_friendly"])[:4]))
+sql = "INSERT INTO info_pc (hostname, OS_NAME, uptime, kernel, CPUname, CPUfrequency, datetime, total, used, free) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+val = (str(socket.gethostname()), str(platform.system()), float(seconds_elapsed()), str(platform.release()), str(cpuinfo.get_cpu_info()[
+       "brand_raw"]), float(str(cpuinfo.get_cpu_info()["hz_actual_friendly"])[:4]), time.strftime('%Y-%m-%d %H:%M:%S'), (total // (2**30)), (used // (2**30)), (free // (2**30)))
 print(val)
 mycursor.execute(sql, val)
 
@@ -33,16 +45,17 @@ mydb.commit()
 print(mycursor.rowcount, "record inserted.")
 
 
-platform.system()
-socket.gethostname()
-cpuinfo.get_cpu_info()
-platform.release()
-socket.gethostbyname(socket.gethostname())
-platform.version()
-platform.machine()
+# platform.system()
+# socket.gethostname()
+# cpuinfo.get_cpu_info()
+# platform.release()
+# socket.gethostbyname(socket.gethostname())
+# platform.version()
+# platform.machine()
 
-total, used, free = shutil.disk_usage("/")
+# print("Total: %d GiB" % (total // (2**30)))
+# print("Used: %d GiB" % (used // (2**30)))
+# print("Free: %d GiB" % (free // (2**30)))
 
-print("Total: %d GiB" % (total // (2**30)))
-print("Used: %d GiB" % (used // (2**30)))
-print("Free: %d GiB" % (free // (2**30)))
+status = os.system('systemctl is-active --quiet apache2')
+print(status)  # will return 0 for active else inactive.
